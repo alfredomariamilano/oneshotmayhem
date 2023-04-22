ef05432733b7aa786d81534728eef4bad7339bd6
Alfredo Maria Milano <alfmarmil@gmail.com>
2023-04-22 15:31:52 +0200

    Changed masonry columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -136,7 +136,7 @@ export default function Home() {
         className={css.masonry}
       >
         <Masonry
-          columns={{ xs: 1, sm: 2, md: 3, lg: 4, xl: 5 }}
+          columns={{ xs: 2, sm: 2, md: 3, lg: 4, xl: 5 }}
           // spacing={{ xs: 0, sm: 0, md: 0 }}
           spacing={2}
           sx={{
